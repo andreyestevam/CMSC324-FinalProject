d3c6372116345aaeddccfa94fa838aca7671c762
dev-JasonYoo <jasonshyoo@gmail.com>
2026-04-24 00:41:21 -0400

    Add model.py where models are stored. Edit 02_train_swin.ipynb

---
+++ final_project/model.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import torch
+
+def model1(*hparams):
+    pass
+
+def model2(*hparams):
+    pass
+
+# NOTE: start a name with underscore(_) to not import
+def _module(*hparams):
+    pass
